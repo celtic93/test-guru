9acf8ad5a1bcd12bd5409ba005042eb35102f22b
celtic93 <egorka80@list.ru>
2018-10-31 22:49:45 +0300

    Add correct attribute to answer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_31_193418) do
+ActiveRecord::Schema.define(version: 2018_10_31_194410) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "correct"
   end
 
   create_table "categories", force: :cascade do |t|
