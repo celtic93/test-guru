348c267cba5ff68327b9a246c1553cbde38e8297
celtic93 <egorka80@list.ru>
2018-11-03 18:15:41 +0300

    Added Test class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+
+  def self.by_category(category)
+    category_id = Category.find_by(title: category).id
+    where(category_id: category_id).order(id: :desc)
+  end
 end
