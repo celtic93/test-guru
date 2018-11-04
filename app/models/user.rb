173f20f8a94e1246cd42f0aa11e7ee2a16fa473e
celtic93 <egorka80@list.ru>
2018-11-04 14:57:09 +0300

    Fixed User instance method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   def tests_by_level(level)
-    test_ids = StartedTest.where(user_id: self.id).pluck(:test_id)
-    Test.where(level: level).where(id: test_ids).pluck(:title)
+    Test.joins("INNER JOIN started_tests ON started_tests.test_id = tests.id")
+        .where("tests.level = ? AND user_id = ?", level, self.id).pluck(:title)
   end
 end
