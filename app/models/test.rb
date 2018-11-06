3dfa82f684953d231b392e157aeabaa708827ab9
Evgeniy Fateev <spaceflow@gmail.com>
2018-11-06 23:55:59 +0300

    Update app/models/test.rb
    Co-Authored-By: celtic93 <42292516+celtic93@users.noreply.github.com>

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,6 @@ class Test < ApplicationRecord
 
   def self.by_category(category)
     Test.joins("INNER JOIN categories ON categories.id = tests.category_id")
-        .where("categories.title = ?", category).order(title: :desc).pluck(:title)
+        .where(categories: { title: category }).order(title: :desc).pluck(:title)
   end
 end
