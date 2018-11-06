43e78e6d63b152b237a7ac3e9a3164ab9d2bd8be
Evgeniy Fateev <spaceflow@gmail.com>
2018-11-06 23:55:15 +0300

    Update app/models/user.rb
    Co-Authored-By: celtic93 <42292516+celtic93@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 
   def tests_by_level(level)
     Test.joins("INNER JOIN started_tests ON started_tests.test_id = tests.id")
-        .where("tests.level = ? AND user_id = ?", level, id).pluck(:title)
+        .where(started_tests: { user_id: id }).where(level: level)
   end
 end
