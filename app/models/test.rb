7e180efb25615f40b478fd21174d4f7eacb0e38f
celtic93 <egorka80@list.ru>
2018-11-08 23:15:13 +0300

    Added scopes

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,8 +5,9 @@ class Test < ApplicationRecord
   belongs_to :category  
   belongs_to :author, class_name: 'User'
 
-  def self.by_category(category)
-    Test.joins("INNER JOIN categories ON categories.id = tests.category_id")
-        .where(categories: { title: category }).order(title: :desc).pluck(:title)
-  end
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :by_category, -> (category) { joins("INNER JOIN categories ON categories.id = tests.category_id")
+                                      .where(categories: { title: category }).order(title: :desc).pluck(:title) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
   has_many :tests, through: :started_tests
   has_many :created_tests, class_name: 'Test', foreign_key: :author_id
 
-  def tests_by_level(level)
-    Test.joins("INNER JOIN started_tests ON started_tests.test_id = tests.id")
-        .where(started_tests: { user_id: id }).where(level: level)
-  end
+  scope :tests_by_level, -> (level) { joins("INNER JOIN started_tests ON started_tests.test_id = tests.id")
+                                      .where(started_tests: { user_id: id }).where(level: level) }
 end
