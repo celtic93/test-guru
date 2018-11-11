9ea969aea09e6ae8e0a4119cbf40c5b11c45e8f7
celtic93 <egorka80@list.ru>
2018-11-11 23:07:17 +0300

    Fixed Answer validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,7 +2,7 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :validate_answers_count
+  validate :validate_answers_count, on: :create
 
   scope :correct, -> { where(correct: true) }
 
