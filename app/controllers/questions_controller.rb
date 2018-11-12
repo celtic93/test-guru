f8322d64742f3e6c4f253b441a34111c66acea37
celtic93 <egorka80@list.ru>
2018-11-12 09:40:22 +0300

    Added callbacks and exception to QuestionController

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,17 @@
 class QuestionsController < ApplicationController
 
+  before_action :test_find, only: [:index]
+  before_action :question_find, only: [:show, :delete]
+
+  rescue_from ActiveRecord::RecordNotFound,
+    with: :rescue_question_not_found
+
   def index
-    test = Test.find(params[:test_id])
-    render plain: test.questions.inspect
+    render plain: @test.questions.inspect
   end
 
-  def show
-    question = Question.find(params[:id])
-    render plain: question.inspect    
+  def show    
+    render plain: @question.inspect    
   end
 
   def new
@@ -19,9 +23,26 @@ class QuestionsController < ApplicationController
     render plain: question.inspect
   end
 
+  def delete
+    @question.destroy
+    render plain: 'The question was deleted from test'
+  end
+
   private
 
   def question_params
     params.permit(:body, :test_id)
+  end
+
+  def test_find
+    @test = Test.find(params[:test_id])
+  end
+
+  def question_find
+    @question = Question.find(params[:id])
+  end
+
+  def rescue_question_not_found
+    render plain: 'The question not found'
   end
 end
