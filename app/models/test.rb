e2812877da22342846865fda1642ee99605de937
celtic93 <egorka80@list.ru>
2018-11-20 23:52:09 +0300

    Added TestPassage model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
   has_many :questions
-  has_many :started_tests
-  has_many :users, through: :started_tests
+  has_many :test_passages
+  has_many :users, through: :test_passages
   belongs_to :category  
   belongs_to :author, class_name: 'User'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :started_tests
-  has_many :tests, through: :started_tests
+  has_many :test_passages
+  has_many :tests, through: :test_passages
   has_many :created_tests, class_name: 'Test', foreign_key: :author_id
 
   validates :email, presence: true
