d9233dbab96bcf06a85503de336fab3c13f5f893
celtic93 <egorka80@list.ru>
2018-11-20 17:14:16 +0300

    Added Answer scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
+  
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true, except: :index
+    end
   end
 end
