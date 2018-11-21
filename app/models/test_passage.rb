51014babd385c5af296c8f15524c4e117ecd8912
celtic93 <egorka80@list.ru>
2018-11-21 12:08:20 +0300

    Added TestPassage realisation

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -2,4 +2,37 @@ class TestPassage < ApplicationRecord
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
+
+  before_validation :before_validation_set_first_question, on: :create
+
+  def accept!(answer_ids)
+    if correct_answer?(answer_ids)
+      self.correct_question += 1
+    end
+
+    self.current_question = next_question
+    save!
+  end
+
+  def completed?
+    current_question.nil?
+  end
+
+  private
+
+  def before_validation
+    self.current_question = test.question.first if test.present?
+  end
+
+  def correct_answer?(answer_ids)
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+  end
+
+  def correct_answers
+    current_question.answers.correct
+  end
+
+  def next_question
+    test.question.order(:id).where('id > ?', current_question.id).first
+  end
 end
