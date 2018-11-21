d23e38cfb9e5781aaba3afec8d89c398a8f4a087
celtic93 <egorka80@list.ru>
2018-11-21 15:07:20 +0300

    Update seed.rb

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,13 +4,14 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  after_validation :after_validation_set_next_question, on: :update
 
   def accept!(answer_ids)
     if correct_answer?(answer_ids)
-      self.correct_question += 1
+      self.correct_questions += 1
     end
 
-    self.current_question = next_question
+    #next_question
     save!
   end
 
@@ -32,7 +33,7 @@ class TestPassage < ApplicationRecord
     current_question.answers.correct
   end
 
-  def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
+  def after_validation_set_next_question
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
