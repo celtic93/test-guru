1b7f1316682eff01864b4fe720314c1447bac75a
celtic93 <egorka80@list.ru>
2018-11-27 02:15:52 +0300

    Added part1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
+    @user = User.find_by(email: params[:email])
 
-    if user&.authenticate(params[:password])
-      session[:user_id] = user.id
+    if @user&.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to tests_path
     else
-      flash.now[:alert] = 'Are you Guru? Verify your Email and Passwor please'
+      flash.now[:alert] = 'Are you Guru? Verify your Email and Password please'
       render :new
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'tests#index'
+
   get :signup, to: 'users#new'
   get :login, to: 'sessions#new'
 
