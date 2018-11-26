ea3d8c2b8a7815d098d65d15cce8767e20ec82ce
celtic93 <egorka80@list.ru>
2018-11-27 00:38:08 +0300

    Added flash alert

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to tests_path
     else
+      flash.now[:alert] = 'Are you Guru? Verify your Email and Passwor please'
       render :new
     end
   end
