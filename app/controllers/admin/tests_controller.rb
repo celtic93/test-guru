ddbc86ad07dadc56b96f483097d5726969a69a51
celtic93 <egorka80@list.ru>
2018-11-29 22:19:32 +0300

    Added Admin resources

---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,51 @@
+class Admin::TestsController < Admin::BaseController
+  
+  before_action :find_test, only: %i[show start update destroy]
+
+  def index
+    @tests = Test.all
+  end
+
+  def show
+  end
+
+  def new
+    @test = current_user.tests.new
+  end
+
+  def create
+    @test = current_user.tests.new(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to admin_test_path(@test)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to admin_tests_path
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id)
+  end
+
+  def find_test
+    @test = Test.find(params[:id])    
+  end
+end
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,13 +1,9 @@
 class TestsController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_test, only: %i[show start]
+  before_action :find_test, only: %i[start]
 
   def index
     @tests = Test.all
-  end
-
-  def show
-    
   end
 
   def start
