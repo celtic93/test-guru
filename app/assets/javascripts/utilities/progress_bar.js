cfb4b1ee5ad0eb3bd16b9be1198825a7e4219fd0
celtic93 <egorka80@list.ru>
2018-12-18 23:14:10 +0300

    Fixed progress bar

--- app/assets/javascripts/utilities/progress_bar.js
+++ app/assets/javascripts/utilities/progress_bar.js
@@ -3,7 +3,7 @@ document.addEventListener('turbolinks:load', function() {
 
   if (bar) {
     var barStep = 1/bar.dataset.count
-    var steps = bar.dataset.questionIndex
+    var steps = bar.dataset.questionIndex - 1
 
     bar.style.width = barStep * steps * 100 + '%'
   }  
