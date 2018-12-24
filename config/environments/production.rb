ff171ab15bfdf660d9440befce0149005f80632e
celtic93 <egorka80@list.ru>
2018-12-24 20:17:15 +0300

    Fixed mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'lit-scrubland-65881.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    adress:               'smtp.gmail.com',
+    address:               'smtp.gmail.com',
     port:                 587,
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
