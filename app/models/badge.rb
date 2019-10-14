a2399285c96281b86bb72b1bc7ac765d6e529d1a
celtic93 <egorka80@list.ru>
2019-10-14 22:00:43 +0300

    Small fixes added

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -3,8 +3,4 @@ class Badge < ApplicationRecord
   has_many :users, through: :user_badge
   belongs_to :rule
   validates :name, presence: true
-
-  # def suitable?(test_passage)
-  #   rule.passes?(test_passage)
-  # end
 end
